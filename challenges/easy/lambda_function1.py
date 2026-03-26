ec1bb069066cd172f4b28fc75006fab96e16d8ce
Sanya Baghla <157266452+silentstormSanya@users.noreply.github.com>
2026-03-26 19:24:53 -0400

    Fix key name from 'temperature' to 'celsius'

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -8,7 +8,7 @@ Expected output: {"statusCode": 200, "body": 77}
 import json
 
 def lambda_handler(event, context=None):
-    celsius = event['temperature']
+    celsius = event['celsius']
     
     if celsius == None:
         return {
