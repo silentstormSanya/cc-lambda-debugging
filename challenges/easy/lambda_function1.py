92eea31fdc6b1c4294a73591a84130daa4a9c517
Sanya Baghla <157266452+silentstormSanya@users.noreply.github.com>
2026-03-26 20:19:43 -0400

    changes to easy-2

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -5,22 +5,21 @@ Convert Celsius to Fahrenheit
 Expected input: {"temperature": 25}
 Expected output: {"statusCode": 400, "body": 77}
 """
+
+
 import json
 
 def lambda_handler(event, context=None):
-    celsius = event['temperature']
-    
-    if celsius == None:
+    if 'temperature' not in event or event['temperature'] is None:
         return {
             'statusCode': 400,
             'body': json.dumps('Error: temperature field is required')
         }
-        
+
     celsius = event['temperature']
     fahrenheit = celsius * 9/5 + 32
-    fahrenheit = int(fahrenheit)
-    
+
     return {
-        'statusCode': 400,
+        'statusCode': 200,
         'body': fahrenheit
     }
